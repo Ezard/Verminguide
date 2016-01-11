02c7a190c7ac6fd5df101b274c5340fc4a9b1b90
Ezard <ezard.ben@gmail.com>
2016-01-11 14:02:40 +0000

    Added enemy stuff to API

--- api/api.js
+++ api/api.js
@@ -1,5 +1,48 @@
 module.exports = function (router, con) {
 	return {
+		getEnemies: function (callback) {
+			con.query("SELECT name, description, notes FROM enemies", function (err, rows, fields) {
+				res.set("Content-type", "application/json");
+				var enemies = [];
+				for (var i = 0; i < rows.length; i++) {
+					var obj = {};
+					obj.name = rows[i].name;
+					obj.description = rows[i].description;
+					obj.notes = [];
+					var split = rows[i].notes.split("&&");
+					for (var j = 0; j < split.length; j++) {
+						var split2 = split[j].split("||");
+						obj.notes.push({title: split2[0], content: split2[1]});
+					}
+					obj.icon = "http://images.vermintideutility.com/enemies/icons/" + obj.name.toLowerCase().replace(/ /g, "_") + ".png";
+					obj.image = "http://images.vermintideutility.com/enemies/" + obj.name.toLowerCase().replace(/ /g, "_") + ".png";
+					enemies.push(obj);
+				}
+				callback(enemies);
+			});
+		},
+		getEnemy: function(name, callback) {
+			con.query("SELECT name, description, notes FROM enemies WHERE name=" + con.escape(name), function (err, rows, fields) {
+				res.set("Content-type", "application/json");
+				if (rows.length == 0) {
+					callback(null);
+				} else {
+					var enemy = {};
+					enemy.name = rows[0].name;
+					enemy.description = rows[0].description;
+					enemy.notes = [];
+					var split = rows[0].notes.split("&&");
+					for (var i = 0; i < split.length; i++) {
+						var split2 = split[i].split("||");
+						enemy.notes.push({title: split2[0], content: split2[1]});
+					}
+					enemy.icon = "http://images.vermintideutility.com/enemies/icons/" + enemy.name.toLowerCase().replace(/ /g, "_") + ".png";
+					enemy.image = "http://images.vermintideutility.com/enemies/" + enemy.name.toLowerCase().replace(/ /g, "_") + ".png";
+					callback(enemy);
+				}
+			});
+		},
+
 		getTrinkets: function (callback) {
 			con.query("SELECT t.name, tt.description as description, tt.name as type, effect, r.name as rarity, image FROM trinkets t LEFT JOIN trinket_types tt ON t.type=tt.id LEFT JOIN rarities r ON t.rarity=r.id", function (err, rows, fields) {
 				var trinkets = [];

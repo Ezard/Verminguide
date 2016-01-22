21382619178a99c1ce38534d3536e026a2e902be
Ezard <ezard.ben@gmail.com>
2016-01-22 16:38:39 +0000

    Moved header-setting code into separate function

--- api/router.js
+++ api/router.js
@@ -2,14 +2,14 @@ module.exports = function (router, api) {
 
 	router.get('/enemies', function (req, res) {
 		api.getEnemies(function (enemies) {
-			res.set("Content-type", "application/json");
+			setHeaders(res);
 			res.end(JSON.stringify(enemies));
 		});
 	});
 
 	router.get("/enemies/:name([a-zA-Z',\-\s]+)", function (req, res) {
 		api.getEnemy(req.params.name.replace(/-/g, ' '), function (enemy) {
-			res.set("Content-type", "application/json");
+			setHeaders(res);
 			if (enemy == null) {
 				res.status(404).end(JSON.stringify({"error": {"message": "Sorry, that resource does not exist"}}));
 			} else {
@@ -20,21 +20,21 @@ module.exports = function (router, api) {
 
 	router.get('/heroes', function (req, res) {
 		api.getHeroes(function (heroes) {
-			res.set("Content-type", "application/json");
+			setHeaders(res);
 			res.end(JSON.stringify(heroes));
 		})
 	});
 
 	router.get('/trinkets', function (req, res) {
 		api.getTrinkets(function (trinkets) {
-			res.set("Content-type", "application/json");
+			setHeaders(res);
 			res.end(JSON.stringify(trinkets));
 		});
 	});
 
 	router.get("/trinkets/:name([a-zA-Z',\-\s]+)", function (req, res) {
 		api.getTrinket(req.params.name.replace(/-/g, ' '), function (trinket) {
-			res.set("Content-type", "application/json");
+			setHeaders(res);
 			if (trinket == null) {
 				res.status(404).end(JSON.stringify({"error": {"message": "Sorry, that resource does not exist"}}));
 			} else {
@@ -45,14 +45,14 @@ module.exports = function (router, api) {
 
 	router.get('/weapons', function (req, res) {
 		api.getWeapons(function (weapons) {
-			res.set("Content-type", "application/json");
+			setHeaders(res);
 			res.end(JSON.stringify(weapons));
 		});
 	});
 
 	router.get("/weapons/:name([a-zA-Z',\-\s]+)", function (req, res) {
 		api.getWeaponsByClass(req.params.name.replace(/-/g, ' '), function (trinket) {
-			res.set("Content-type", "application/json");
+			setHeaders(res);
 			if (trinket == null) {
 				res.status(404).end(JSON.stringify({"error": {"message": "Sorry, that resource does not exist"}}));
 			} else {
@@ -61,3 +61,7 @@ module.exports = function (router, api) {
 		});
 	});
 };
+
+function setHeaders(res) {
+	res.set("Content-type", "application/json");
+}

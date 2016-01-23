902afbcb020751a1988f4a01ef94f1f2b6b7535b
Ezard <ezard.ben@gmail.com>
2016-01-23 02:57:20 +0000

    Code reduction, improvement and fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,15 @@
 window.addEventListener('load', function () {
 	document.body.addEventListener('click', function (event) {
-		var type = "";
 		var target = event.target;
 		if (!target.href) target = target.parentNode;
-		if (target.href && event.button == 0 && target.origin == document.location.origin && window.history && history.pushState) {
+		if (target.href && event.button == 0/* && target.origin == document.location.origin*/ && window.history && history.pushState) {
 			event.preventDefault();
 			document.getElementById("content").style.opacity = 0.5;
-			var xhr = new XMLHttpRequest();
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState == 4) {
-					var json = JSON.parse(xhr.responseText);
-					var result;
-					console.log(type);
-					if (type == "trinkets") {
-						 result = Handlebars.templates.trinkets(json);
-					}
-					if (type == "enemy") {
-						result = Handlebars.templates.enemy(json);
-					}
-					document.getElementById("content").innerHTML = result;
-					document.getElementById("content").style.opacity = 1;
-					window.history.pushState({}, "", document.location.origin + "/" + path);
-				}
-			};
-			var path;
-			var match;
-			console.log(target.href.replace("http://", "http://api."));
-			var page = target.href.replace("http://vermintideutility.com/", "").replace("http://localhost/", "");
-			console.log("page = " + page);
-			if (/enemies\/?/.test(page)) {
-				if (match = page.match(/enemies\/([a-zA-Z',\-\s]+)\/?$/)) {
-					console.log(match);
-					path = "enemies/" + match[1];
-					type = "enemy";
-				} else {
-					path = "enemies/";
-					type = "enemies";
-				}
-			}
-			//xhr.open("GET", "http://api.localhost/" + path, true);
-			var s = "http://api.vermintideutility.com/" + path;
-			console.log(s);
-			xhr.open("GET", s, true);
-			xhr.send();
+			getContent(target.href, function(html) {
+				document.getElementById("content").innerHTML = html.replace(/<h1>.*<\/h1>/);
+				document.getElementById("content").style.opacity = 1;
+				window.history.pushState({}, "", target.href);
+			});
 		}
 	});
 }, false);
@@ -53,6 +20,25 @@ window.onpopstate = function (event) {
 		console.log(document.location);
 	}
 };
+
+function getContent(url, callback) {
+	var type;
+	if (/enemies\/?/.test(url)) {
+		if (/enemies\/[a-zA-Z',\-\s]+\/?$/.test(url)) {
+			type = "enemy";
+		} else {
+			type = "enemies";
+		}
+	}
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			callback(Handlebars.templates[type](JSON.parse(xhr.responseText)));
+		}
+	};
+	xhr.open("GET", url.replace("http://", "http://api."), true);
+	xhr.send();
+}
 
 function setMenuOpen(open) {
 	if (open) {

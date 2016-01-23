ab4cdc76e3efae1bb33018a4ad26cacc8bdca67b
Ezard <ezard.ben@gmail.com>
2016-01-23 04:00:09 +0000

    General work

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,30 @@ window.addEventListener('load', function () {
 	document.body.addEventListener('click', function (event) {
 		var target = event.target;
 		if (!target.href) target = target.parentNode;
-		if (target.href && event.button == 0/* && target.origin == document.location.origin*/ && window.history && history.pushState) {
+		if (target.href && event.button == 0 && target.origin == document.location.origin && window.history && history.pushState) {
 			event.preventDefault();
-			document.getElementById("content").style.opacity = 0.5;
-			getContent(target.href, function(html) {
-				document.getElementById("content").innerHTML = html.replace(/<h1>.*<\/h1>/);
-				document.getElementById("content").style.opacity = 1;
-				window.history.pushState({}, "", target.href);
-			});
+			setContent(target.href);
 		}
 	});
 }, false);
 
 window.onpopstate = function (event) {
 	if (window.history && history.pushState) {
-//			event.preventDefault();
+		event.preventDefault();
+		//setContent();
+		console.log(event);
 		console.log(document.location);
 	}
 };
+
+function setContent(url) {
+	document.getElementById("content").style.opacity = 0.5;
+	getContent(url, function(html) {
+		document.getElementById("content").innerHTML = html;
+		document.getElementById("content").style.opacity = 1;
+		window.history.pushState({}, "", url);
+	});
+}
 
 function getContent(url, callback) {
 	var type;

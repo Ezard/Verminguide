9308d468d0657fda595f0ace87a8d086e8cdbb70
Ezard <ezard.ben@gmail.com>
2016-01-24 20:36:27 +0000

    Updated hero listing styling

--- scripts/templates.js
+++ scripts/templates.js
@@ -70,15 +70,19 @@ templates['enemy'] = template({"1":function(container,depth0,helpers,partials,da
     + "</td>\r\n				<td>Coming soon!</td>\r\n			</tr>\r\n			</tbody>\r\n		</table>\r\n	</div>\r\n</div>";
 },"useData":true});
 templates['heroes'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "		<a href=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\"><img src=\""
+    + "\">\r\n			<div>\r\n				<div style=\"background-image:url("
     + alias4(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"icon","hash":{},"data":data}) : helper)))
-    + "\" alt=\""
+    + ")\"></div>\r\n				<h2 "
+    + ((stack1 = (helpers.smallerFont || (depth0 && depth0.smallerFont) || alias2).call(alias1,(depth0 != null ? depth0.name : depth0),{"name":"smallerFont","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "\"/></a>\r\n";
+    + "</h2>\r\n			</div>\r\n		</a>\r\n";
+},"2":function(container,depth0,helpers,partials,data) {
+    return "";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 

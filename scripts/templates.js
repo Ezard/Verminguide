ce64fbad4d944b106c3ffb85b49370cfc8835692
Ezard <ezard.ben@gmail.com>
2016-01-25 18:44:02 +0000

    Enemy page now shows attacks with damage amounts

--- scripts/templates.js
+++ scripts/templates.js
@@ -32,6 +32,42 @@ templates['enemy'] = template({"1":function(container,depth0,helpers,partials,da
     + "</h4>\r\n				<p>"
     + alias4(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"content","hash":{},"data":data}) : helper)))
     + "</p>\r\n";
+},"3":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "						<th>"
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
+    + "</th>\r\n";
+},"5":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "						<th>"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.damage : depth0)) != null ? stack1.easy : stack1), depth0))
+    + "</th>\r\n";
+},"7":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "						<th>"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.damage : depth0)) != null ? stack1.normal : stack1), depth0))
+    + "</th>\r\n";
+},"9":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "						<th>"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.damage : depth0)) != null ? stack1.hard : stack1), depth0))
+    + "</th>\r\n";
+},"11":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "						<th>"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.damage : depth0)) != null ? stack1.nightmare : stack1), depth0))
+    + "</th>\r\n";
+},"13":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "						<th>"
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.damage : depth0)) != null ? stack1.cataclysm : stack1), depth0))
+    + "</th>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : {};
 
@@ -53,21 +89,33 @@ templates['enemy'] = template({"1":function(container,depth0,helpers,partials,da
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.description : stack1), depth0))
     + "</p>\r\n		<div>\r\n			<h3 id=\"notes\">Notes</h3>\r\n"
     + ((stack1 = helpers.each.call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.notes : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "		</div>\r\n	</div>\r\n	<div class=\"stats\">\r\n		<h3 id=\"stats\">Stats</h3>\r\n		<table>\r\n			<tbody>\r\n			<tr>\r\n				<td class=\"tooltip\"\r\n					title=\"Whether or not this enemy is protected by armour. Armoured enemies can only be damaged by attacks to the head or by strikes with the 'Armour Penetration' attribute. All weapons have this attribute on their heavy attack, but some weapons such as axes have it by default\">\r\n					Armoured:\r\n				</td>\r\n				<td>"
+    + "		</div>\r\n	</div>\r\n	<div class=\"stats\">\r\n		<h3 id=\"stats\">Stats</h3>\r\n		<table>\r\n			<tbody>\r\n			<tr>\r\n				<td class=\"tooltip\"\r\n				    title=\"Whether or not this enemy is protected by armour. Armoured enemies can only be damaged by attacks to the head or by strikes with the 'Armour Penetration' attribute. All weapons have this attribute on their heavy attack, but some weapons such as axes have it by default\">\r\n					Armoured:\r\n				</td>\r\n				<td>"
     + alias2((helpers.booleanToYesNo || (depth0 && depth0.booleanToYesNo) || helpers.helperMissing).call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.armoured : stack1),{"name":"booleanToYesNo","hash":{},"data":data}))
-    + "</td>\r\n			</tr>\r\n			<tr>\r\n				<td class=\"tooltip\"\r\n					title=\"Every time the poison DoT ticks, enemies have a chance to die instantly, regardless of their remaining health. The percentage chance for this to happen is (100 - resistance). Some enemies have 100 poison resistance, meaning that this instant death effect can never happen to them\">\r\n					Poison Resistance:\r\n				</td>\r\n				<td>"
+    + "</td>\r\n			</tr>\r\n			<tr>\r\n				<td class=\"tooltip\"\r\n				    title=\"Every time the poison DoT ticks, enemies have a chance to die instantly, regardless of their remaining health. The percentage chance for this to happen is (100 - resistance). Some enemies have 100 poison resistance, meaning that this instant death effect can never happen to them\">\r\n					Poison Resistance:\r\n				</td>\r\n				<td>"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.poison_resistance : stack1), depth0))
-    + "</td>\r\n			</tr>\r\n			</tbody>\r\n		</table>\r\n		<div>\r\n			<table class=\"difficulties\">\r\n				<tbody>\r\n				<tr>\r\n					<th class=\"tooltip\" title=\"The difficulty setting\">Difficulty</th>\r\n					<th class=\"tooltip\" title=\"The amount of health that the enemy has on the corresponding difficulty setting\">Health</th>\r\n					<th class=\"tooltip\">Damage</th>\r\n				</tr>\r\n				<tr>\r\n					<td>Easy</td>\r\n					<td>"
+    + "</td>\r\n			</tr>\r\n			</tbody>\r\n		</table>\r\n		<div>\r\n			<table class=\"difficulties\">\r\n				<tbody>\r\n				<tr>\r\n					<th class=\"tooltip\" title=\"The difficulty setting\">Difficulty</th>\r\n					<th class=\"tooltip\" title=\"The amount of health that the enemy has on the corresponding difficulty setting\">Health</th>\r\n"
+    + ((stack1 = helpers.each.call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.attacks : stack1),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</tr>\r\n				<tr>\r\n					<td>Easy</td>\r\n					<td>"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.hp : stack1)) != null ? stack1.easy : stack1), depth0))
-    + "</td>\r\n					<td>Coming soon!</td>\r\n				</tr>\r\n				<tr>\r\n					<td>Normal</td>\r\n					<td>"
+    + "</td>\r\n"
+    + ((stack1 = helpers.each.call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.attacks : stack1),{"name":"each","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</tr>\r\n				<tr>\r\n					<td>Normal</td>\r\n					<td>"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.hp : stack1)) != null ? stack1.normal : stack1), depth0))
-    + "</td>\r\n					<td>Coming soon!</td>\r\n				</tr>\r\n				<tr>\r\n					<td>Hard</td>\r\n					<td>"
+    + "</td>\r\n"
+    + ((stack1 = helpers.each.call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.attacks : stack1),{"name":"each","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</tr>\r\n				<tr>\r\n					<td>Hard</td>\r\n					<td>"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.hp : stack1)) != null ? stack1.hard : stack1), depth0))
-    + "</td>\r\n					<td>Coming soon!</td>\r\n				</tr>\r\n				<tr>\r\n					<td>Nightmare</td>\r\n					<td>"
+    + "</td>\r\n"
+    + ((stack1 = helpers.each.call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.attacks : stack1),{"name":"each","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</tr>\r\n				<tr>\r\n					<td>Nightmare</td>\r\n					<td>"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.hp : stack1)) != null ? stack1.nightmare : stack1), depth0))
-    + "</td>\r\n					<td>Coming soon!</td>\r\n				</tr>\r\n				<tr>\r\n					<td>Cataclysm</td>\r\n					<td>"
+    + "</td>\r\n"
+    + ((stack1 = helpers.each.call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.attacks : stack1),{"name":"each","hash":{},"fn":container.program(11, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</tr>\r\n				<tr>\r\n					<td>Cataclysm</td>\r\n					<td>"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.hp : stack1)) != null ? stack1.cataclysm : stack1), depth0))
-    + "</td>\r\n					<td>Coming soon!</td>\r\n				</tr>\r\n				</tbody>\r\n			</table>\r\n		</div>\r\n	</div>\r\n</div>";
+    + "</td>\r\n"
+    + ((stack1 = helpers.each.call(alias3,((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.attacks : stack1),{"name":"each","hash":{},"fn":container.program(13, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</tr>\r\n				</tbody>\r\n			</table>\r\n		</div>\r\n	</div>\r\n</div>";
 },"useData":true});
 templates['heroes'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
@@ -105,9 +153,6 @@ templates['trinkets'] = template({"1":function(container,depth0,helpers,partials
   return "<h1>Trinkets</h1>\r\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.trinkets : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
-templates['weapon'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
-},"useData":true});
 templates['weapons'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
@@ -139,7 +184,7 @@ templates['layouts/main'] = template({"1":function(container,depth0,helpers,part
 
   return "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n	<title>Vermintide Utility"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.title : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "</title>\r\n	<meta name=\"description\" content=\"Everything you could possibly want to know about Vermintide, plus more!\">\r\n	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n	<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">\r\n	<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/main.css\">\r\n	<script src=\"/handlebars/handlebars.js\"></script>\r\n	<script src=\"/scripts/main.js\"></script>\r\n	<script src=\"/scripts/templates.js\"></script>\r\n	<script>(function (i, s, o, g, r, a, m) {\r\n		i['GoogleAnalyticsObject'] = r;\r\n		i[r] = i[r] || function () {\r\n					(i[r].q = i[r].q || []).push(arguments)\r\n				}, i[r].l = 1 * new Date();\r\n		a = s.createElement(o), m = s.getElementsByTagName(o)[0];\r\n		a.async = 1;\r\n		a.src = g;\r\n		m.parentNode.insertBefore(a, m)\r\n	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');\r\n	ga('create', 'UA-72698344-1', 'auto');\r\n	ga('send', 'pageview');</script>\r\n</head>\r\n<body>\r\n<nav id=\"menu\">\r\n	<div id=\"cover\" onclick=\"setMenuOpen(false)\"></div>\r\n	<div id=\"items\">\r\n		<img src=\"/images/logo.png\" alt=\"Vermintide Utility logo\"/>\r\n		<a href=\"/heroes/\">\r\n			Heroes\r\n		</a>\r\n		<a href=\"/enemies/\">\r\n			Enemies\r\n		</a>\r\n		<a href=\"/trinkets/\">\r\n			Trinkets\r\n		</a>\r\n	</div>\r\n</nav>\r\n<div id=\"content\">\r\n	"
+    + "</title>\r\n	<meta name=\"description\" content=\"Everything you could possibly want to know about Vermintide, plus more!\">\r\n	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n	<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">\r\n	<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/main.css\">\r\n	<script src=\"/handlebars/handlebars.js\"></script>\r\n	<script src=\"/scripts/main.js\"></script>\r\n	<script src=\"/scripts/templates.js\"></script>\r\n	<script>(function (i, s, o, g, r, a, m) {\r\n		i['GoogleAnalyticsObject'] = r;\r\n		i[r] = i[r] || function () {\r\n					(i[r].q = i[r].q || []).push(arguments)\r\n				}, i[r].l = 1 * new Date();\r\n		a = s.createElement(o), m = s.getElementsByTagName(o)[0];\r\n		a.async = 1;\r\n		a.src = g;\r\n		m.parentNode.insertBefore(a, m)\r\n	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');\r\n	ga('create', 'UA-72698344-1', 'auto');\r\n	ga('send', 'pageview');</script>\r\n</head>\r\n<body>\r\n<nav id=\"menu\">\r\n	<div id=\"cover\" onclick=\"setMenuOpen(false)\"></div>\r\n	<div id=\"items\">\r\n		<img src=\"/images/logo.png\" alt=\"Vermintide Utility logo\"/>\r\n		<a href=\"/heroes/\">\r\n			Heroes\r\n		</a>\r\n		<a href=\"/enemies/\">\r\n			Enemies\r\n		</a>\r\n		<a href=\"/trinkets/\">\r\n			Trinkets\r\n		</a>\r\n		<a href=\"/weapons/\">\r\n			Weapons\r\n		</a>\r\n	</div>\r\n</nav>\r\n<div id=\"content\">\r\n	"
     + ((stack1 = ((helper = (helper = helpers.body || (depth0 != null ? depth0.body : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"body","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "\r\n</div>\r\n<div id=\"hamburger\" onclick=\"setMenuOpen(true)\">\r\n	<div style=\"margin: 0;\"></div>\r\n	<div></div>\r\n	<div></div>\r\n</div>\r\n</body>\r\n</html>";
 },"useData":true});
